78421950c880df4984403a6c3827884ab3d1b807
Nitin <nitinrajesh094@gmail.com>
2022-09-28 23:53:33 +0530

    ProfileDetail completed

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import DashBoardBoxLayout from "./components/DashBoardBoxLayout";
 import DashBoxContent from "./components/DashBoxContent";
 import SideNavbar from "./components/SideNavbar";
 import TopNavbar from "./components/TopNavbar";
+import { BsPerson } from "react-icons/bs";
+import Image from "next/image";
+import profile from "../assets/man.png";
 
 export default function Home() {
   return (
@@ -11,6 +14,23 @@ export default function Home() {
       <div className={styles.subContainer}>
         <SideNavbar />
         <div className={styles.rightContainer}>
+          <DashBoardBoxLayout>
+            <div className={styles.profileDetailContainer}>
+              <div className={styles.profilePic}>
+                <Image
+                  src={profile}
+                  alt="Picture of the author"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className={styles.profileDetails}>
+                <h2>Nitin Rajesh</h2>
+                <h5>Frontend Developer at XXXXX company</h5>
+                <p>India</p>
+              </div>
+            </div>
+          </DashBoardBoxLayout>
           <DashBoardBoxLayout
             heading="Recommended Jobs"
             subHeading={
@@ -25,7 +45,7 @@ export default function Home() {
               posted="3days"
             />
             <DashBoxContent
-              role="React.Js Developer"
+              role="Frontend Intern"
               companyName="XXXXX"
               location="Remote"
               stipendRange="₹2000-₹10000"
